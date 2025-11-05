9ba88c3cf2b20c8c75a2db1da95defee0a549548
Yogesh Sanchania <yogesh.sanchania@gmail.com>
2025-11-06 00:13:23 +0530

    added reverse proxy for api

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:3000/:path*"
+        destination: `${process.env.NESTJS_API_URL_PROD}/:path*`
       }
     ]
   },
