f3ee3478bb111ed2cd9a14fe27d8a9e3cceb835e
Yogesh Sanchania <yogesh.sanchania@gmail.com>
2025-11-05 12:50:02 +0530

    added cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:4000',
+    origin: [
+      'http://localhost:4000',
+      'https://todo-app-psi-rosy.vercel.app'
+    ],
     credentials: true
   });
   
