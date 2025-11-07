65b0bd7c9890aa2ddfd9463dfd2b0edbceb14815
Yogesh Sanchania <yogesh.sanchania@gmail.com>
2025-11-07 22:14:11 +0530

    fix loader

--- frontend/components/TodoList.tsx
+++ frontend/components/TodoList.tsx
@@ -117,7 +117,7 @@ export default function TodoList({ todos, status }: TodoListProps) {
     return (
         <>
             {loading && (
-                <div className="fixed inset-0 bg-gray-500 flex items-center justify-center z-50">
+                <div className="fixed inset-0 bg-gray-300 opacity-5 flex items-center justify-center z-50">
                     <div className="loader"></div>
                 </div>
             )}
